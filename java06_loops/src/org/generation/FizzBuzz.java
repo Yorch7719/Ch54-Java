e657ef47fd99664063bd821bacb1ce4ea5c61a8d
Jorge Aragon <jorge.aragon19@gmail.com>
2025-06-19 09:34:32 -0600

    Act.

--- java06_loops/src/org/generation/FizzBuzz.java
+++ java06_loops/src/org/generation/FizzBuzz.java
@@ -5,7 +5,6 @@ public class FizzBuzz {
 	public static void main(String[] args) {
 		/*
 		 * Escribir un programa que muestre en pantalla los números del 1 al 20,
-<<<<<<< HEAD
 		 * sustituyendo los múltiplos de 3 por la palabra "fizz", los multiplos de 5 
 		 * por la palabra "buzz" los multiplos de ambos por la palabra "fizz buzz", es decir, 
 		 * los multiplos de 3 y 5.
@@ -48,34 +47,6 @@ public class FizzBuzz {
 		}
 		
 		
-=======
-		 * sustituyendo los múltiplos de 3 por la palabra “fizz”, los múltiplos de 5 por
-		 * la palabra "buzz" los múltiplos de ambos por la palabra "fizzbuzz", es decir,
-		 * los múltiplos de 3 y 5,
-		 * 
-		 * 
-		 * 1 
-		 * 2 
-		 * fizz 
-		 * 4 
-		 * buzz 
-		 * fizz 
-		 * 7 
-		 * 8 
-		 * fizz 
-		 * buzz 
-		 * 11 
-		 * fizz 
-		 * 13 
-		 * 14 
-		 * fizzbuzz 
-		 * 16 
-		 * 17 
-		 * fizz 
-		 * 19 
-		 * buzz
-		 * 
-		 */
 		for(int i = 1; i <= 20; i++) {
 			
 			if(i % 5 == 0 && i % 3 == 0) {
@@ -89,7 +60,7 @@ public class FizzBuzz {
 			}
 			
 		}
->>>>>>> upstream/main
+
 	}
 
 }
